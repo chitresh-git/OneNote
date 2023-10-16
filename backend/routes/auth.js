af29d18b7b7b689fe592c61fecdd0551d552561e
chitresh mourya <chitreshmourya3@gmail.com>
2023-10-16 23:00:51 +0530

    commit of 16.10.23

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express=require("express")
 
 const router=express.Router();
-const {body,validationResult}=require("express-validator")
-
+const {body,validationResult}=require("express-validator") // this is use to validate users information 
 const User=require("../models/user")
+const bcryptjs=require("bcryptjs") // this package helps to generate password hash and add salt feature to it 
+const jwt=require("jsonwebtoken") // this package generates the json web token for our users
+const jwt_secret="cm_code" // this a secret key which helps us to verify json web token 
 
 router.post("/",[
     body('name','enter a valid name').isLength({min:3}), // conditions for validations of user personal information
@@ -26,13 +28,25 @@ router.post("/",[
         return res.status(400).json({error:"sorry this email is already exist "}) // if user exist then we will send bad request 
       }
       
+      const salt=await bcryptjs.genSalt(10) // this will generate salt for users password
+      const scurePass=await bcryptjs.hash(req.body.password,salt) // this will generate hash for the password and add salt to it
+
       user= await User.create({ // saving the data inside the data base 
         name: req.body.name,
-        password: req.body.password,
+        password: scurePass,
         email: req.body.email,
-      }).then(user => res.json(user)).catch(err=>console.log(err));
+      }).catch(err=>console.log(err));
+
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+
+      const authtoken=jwt.sign(data,jwt_secret) // this will generate jwt token which contains data and our secret key 
+
       
-      // res.json(user)
+      res.json({authtoken}) // sending the token back to the client 
       
     }catch(err){
 
