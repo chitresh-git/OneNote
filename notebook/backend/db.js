a0763f1cda5089bc07aa15db8275a5987517aea5
chitresh mourya <chitreshmourya3@gmail.com>
2024-06-15 19:10:38 +0530

    c

--- notebook/backend/db.js
+++ notebook/backend/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const connectTomongo=()=>{
 
     // mongoose.connect('mongodb://127.0.0.1:27017/newNote')
-    mongoose.connect('mongodb://127.0.0.1:27017/newNote', { useNewUrlParser: true, useUnifiedTopology: true });
+    // mongoose.connect('mongodb://127.0.0.1:27017/newNote', { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect('mongodb+srv://chitreshcm:eN9GrUr4Q0FUYu59@cluster0.rzwbtp1.mongodb.net/newNote', { useNewUrlParser: true, useUnifiedTopology: true });
  
     // mongoose.connect('mongodb://0.tcp.in.ngrok.io:17657/newNote')
 
