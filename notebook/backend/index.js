6005229204e63c174bdc32d6e91bc47cdced5b15
chitresh mourya <chitreshmourya3@gmail.com>
2024-06-15 20:37:28 +0530

    cors

--- notebook/backend/index.js
+++ notebook/backend/index.js
@@ -7,7 +7,16 @@ connectTomongo();
 const app = express()
 const port = 3005
 
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: 'https://one-note-zeta.vercel.app/', // Adjust this to your frontend domain
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
+
+
 app.use(express.json()); // this is the middle ware for using req.body in auth.js
 
 // app.get('/', (req, res) => {
